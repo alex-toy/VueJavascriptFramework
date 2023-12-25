f4a1946b756ab09a8a8320b0e306b98eaf12a6d4
alex-toy <alexei.80@hotmail.fr>
2023-12-25 09:26:40 +0100

    components

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {id: 'manuel', name: 'manuel Lorentz', phone: '87643456', email: 'manuel@test.fr' },
+                {id: 'laura', name: 'laura lozano', phone: '87643456', email: 'laura@test.fr' },
+                {id: 'fred', name: 'fred lassoued', phone: '87643456', email: 'fred@test.fr' },
+            ]
+        }
+    },
+}
+</script>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{detailsAreVisible ? 'Hide Details' : 'Show Details' }}</button>
+        <section v-if="detailsAreVisible">
+            <div>Phone : {{ friend.phone }}</div>
+            <div>Email : {{ friend.email }}</div>
+        </section>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                detailsAreVisible : false,
+                friend: {id: 'manuel', name: 'manuel Lorentz', phone: '87643456', email: 'manuel@test.fr' },
+            }
+        },
+        methods: {
+            toggleDetails(){
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    }
+</script>
