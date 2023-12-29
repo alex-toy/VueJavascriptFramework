b38a28beb70f4052b0f86470ac9f607d90a4b44a
alex-toy <alexei.80@hotmail.fr>
2023-12-29 09:32:16 +0100

    refs

--- composition-01/src/App.vue
+++ composition-01/src/App.vue
@@ -2,26 +2,45 @@
   <section class="container">
     <h2>{{ user.name }}</h2>
     <h2>{{ user.age }}</h2>
+    <h2>{{ age }}</h2>
+    <button @click="setNewAge">Change Age</button>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, reactive, isReactive, isRef, toRefs } from 'vue';
 
 export default {
   setup() {
+    const age = ref(23);
+
     const user = reactive({
       name : 'Maximilian',
       age : 34
     });
 
+
+    console.log(isRef(age.value), isReactive(user.value))
+    console.log(isRef(age), isReactive(user))
+
+    const ageRef = toRefs(age);
+    const userRef = toRefs(user);
+    console.log(isRef(ageRef), isReactive(userRef))
+    
+    function setNewAge() {
+      user.age = 32;
+    }
+
     setTimeout(function() {
       user.name = 'Max'
       user.age = 76
+      age.value = 12
     }, 2000)
 
-    return { user : user }
+    return { user : user, age : age, setNewAge : setNewAge }
   },
+  methods: {
+  }
 };
 </script>
 
